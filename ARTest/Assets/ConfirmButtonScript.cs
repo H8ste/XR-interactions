5cbed7199ce580d74076bd161a4905ea928d1595
BurmasGG <guitartard@hotmail.com>
2021-03-11 11:17:24 +0100

    Scene #2

---
+++ ARTest/Assets/ConfirmButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+
+public class ConfirmButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Stopwatch timer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GazeInputTest()
+    {
+        
+        UnityEngine.Debug.Log("On");
+    }
+
+}

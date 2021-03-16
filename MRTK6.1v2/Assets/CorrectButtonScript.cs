4eab6bff9dbde36d4a42f5532b8581b197952a25
BurmasGG <guitartard@hotmail.com>
2021-03-16 10:24:40 +0100

    Made a small scene were the clicker can be used as input to click some butonns in a menu

---
+++ MRTK6.1v2/Assets/CorrectButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorrectButtonScript : MonoBehaviour
+{
+    public Text uiText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ClickedCorrect()
+    {
+        uiText.text = "Correct!";
+    }
+}

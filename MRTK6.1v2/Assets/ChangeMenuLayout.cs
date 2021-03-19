24ef05edf38cd733b4abe62f4bc93ae15e8ed949
BurmasGG <guitartard@hotmail.com>
2021-03-19 14:33:42 +0100

    Script that swithces between two gameobjects when button is clicked. WIP on arrow menu selection.

---
+++ MRTK6.1v2/Assets/ChangeMenuLayout.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenuLayout : MonoBehaviour
+{
+    public GameObject menu1;
+    public GameObject menu2;
+
+    bool menu1Active = true;
+    bool menu2Active = false;
+
+    void Start()
+    {
+        menu2.SetActive(false);
+    }
+
+
+    public void SwitchMenu()
+    {
+        if (menu1Active && !menu2Active)
+        {
+            menu1.SetActive(false);
+            menu1Active = false;
+            menu2.SetActive(true);
+            menu2Active = true;
+        }else if(menu2Active && !menu1Active)
+        {
+            menu2.SetActive(false);
+            menu2Active = false;
+            menu1.SetActive(true);
+            menu1Active = true;
+        }
+    }
+}
---
+++ MRTK6.1v2/Assets/MenuRaycaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+
+public class MenuRaycaster : MonoBehaviour
+{
+    int layerMask = 1 << 10;
+    RaycastHit hit;
+    Interactable menuItem;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if(hit.collider.gameObject.GetComponent<Interactable>())
+            {
+                
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            
+        }
+    }
+}

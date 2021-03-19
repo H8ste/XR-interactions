be520683730383949969907b434682cbb2d55e12
h8ste <nolse16@student.aau.dk>
2021-03-19 14:31:33 +0100

    Working horisontal / vertical head tilt -> missing clicker select on what is pointed at (arrow)

---
+++ MRTK6.1v2/Assets/PositionSelectionPosition.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class PositionSelectionPosition : MonoBehaviour
+{
+
+    private GameObject selectionPositionReference;
+    private GazeGesture gazeScriptReference;
+
+
+    [SerializeField]
+    private Transform horizontalPos;
+    [SerializeField]
+    private Transform verticalPos;
+
+    private bool oldIsHorizontal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectionPositionReference = FindChildWithTag("SelectionPosition", transform);
+        gazeScriptReference = transform.GetComponentInParent<GazeGesture>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (gazeScriptReference.IsHorizontal != oldIsHorizontal)
+        //{
+            switch (gazeScriptReference.IsHorizontal)
+            {
+                case true:
+                    selectionPositionReference.transform.localPosition = horizontalPos.localPosition;
+                    selectionPositionReference.transform.localRotation = horizontalPos.localRotation;
+                    break;
+
+                case false:
+                    selectionPositionReference.transform.localPosition = verticalPos.localPosition;
+                    selectionPositionReference.transform.localRotation = verticalPos.localRotation;
+                    break;
+            }
+        //}
+
+
+        //oldIsHorizontal = gazeScriptReference.IsHorizontal;
+
+
+    }
+
+
+    public GameObject FindChildWithTag(string tag, Transform parent)
+    {
+
+
+        foreach (Transform child in parent)
+        {
+            //Debug.Log(tag + " == " + child.tag);
+            if (child.tag == tag)
+            {
+                //Debug.Log("found child");
+                return child.gameObject;
+            }
+        }
+        return null;
+    }
+
+}

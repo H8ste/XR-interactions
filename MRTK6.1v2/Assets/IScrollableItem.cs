5b6e64ddaf7cd757fb32e83a54608843d6acb4d5
Nicklas Olsen <nicklas.olsen@hotmail.dk>
2021-03-29 09:36:52 +0200

    Added: ScrollabeItem interface, OnClick eventType, and OrderItem type

---
+++ MRTK6.1v2/Assets/IScrollableItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+interface IScrollableItem
+{
+    GameObject ItemPrefab {get;set;}  
+    OnClick OnClick {get;set;} 
+
+    int ScrollableID {get;set;}
+}
---
+++ MRTK6.1v2/Assets/OrderItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderItem: IScrollableItem
+{
+    /* IScrollableItem Properties */
+    private GameObject itemPrefab;
+    public GameObject ItemPrefab {get {return itemPrefab; }set {itemPrefab = value;}}  
+
+    private OnClick onClick;
+    public OnClick OnClick {get {return onClick;} set {onClick = value;}} 
+
+    private int scrollableID;
+    public int ScrollableID {get {return scrollableID;} set {scrollableID = value;}} 
+
+    /* OrderItem Properties */
+
+    // private LockPK locationCode
+    private short remainingStock;
+    private short amountToTake;
+    private string nameOfItem;
+    private short itemID;
+    private bool isScanned;
+}

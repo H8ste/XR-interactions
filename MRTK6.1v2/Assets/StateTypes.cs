4d2b886b8dae0fe8c7599be16f3f3a1e555fdfbe
Nicklas Olsen <nicklas.olsen@hotmail.dk>
2021-03-29 11:32:10 +0200

    StateHandler, missing the respective state classes for relevant calls

---
+++ MRTK6.1v2/Assets/StateTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateType
+{
+    PickingHandler,
+    ManualOrderPickingHandler,
+    OffloadHandler,
+    RestockHandler,   
+}

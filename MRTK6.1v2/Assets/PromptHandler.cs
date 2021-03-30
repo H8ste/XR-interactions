67497c83084f8c1a97f74feb07c81fee7c795840
Burmas <guitartard@hotmail.com>
2021-03-30 14:04:10 +0200

    Added comments to the PromptHandler script.

--- MRTK6.1v2/Assets/PromptHandler.cs
+++ MRTK6.1v2/Assets/PromptHandler.cs
@@ -7,17 +7,21 @@ using UnityEngine;
 
 public class PromptHandler : MonoBehaviour
 {
+    //Dictionary containing the different values for promptoption constructor
     Dictionary<PromptType, PromptOption[]> allPromptOptions;
+    //Dictionairy with the different questions connected to the specific prompts
     Dictionary<PromptType, string> allPromptQuestions;
     Prompt promptOption;
-    //[SerializeField]
+    //Used to load in the necesarry prefab
     GameObject promptOptionPrefab;
+    //Used to save the current instance of the prompt
     GameObject instantiatedPromptObject;
-   // ScrollHandler scrollhandler;
+
 
 
     void Start()
     {
+        //Prefab is loaded 
         promptOptionPrefab = (GameObject)(Resources.Load("Prefabs/PromptPrefab"));
 
 
@@ -29,11 +33,15 @@ public class PromptHandler : MonoBehaviour
         
     }
    
-    
+    /// <summary>
+    /// Constructor for PromptHanlder
+    /// </summary>
     public PromptHandler()
     {
+        
         allPromptOptions = new Dictionary<PromptType, PromptOption[]>();
         allPromptQuestions = new Dictionary<PromptType, string>();
+        //Populates the Dictionaries with prompt types and their corresponding keys
         foreach (PromptType promptType in (PromptType[])Enum.GetValues(typeof(PromptType)))
         {
             allPromptOptions.Add(promptType, GetPromptOptions(promptType));
@@ -41,18 +49,29 @@ public class PromptHandler : MonoBehaviour
         }
 
     }
-    public int ShowPrompt(PromptType type)
+    /// <summary>
+    /// Spawns a prompt based on the specified prompt type
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public int ShowPrompt(PromptType type)//Function is a WIP
     {
+        //Sets the instance of the prompt object to the prefab
         instantiatedPromptObject = Instantiate(promptOptionPrefab);
+        //Accesses the text element of the prefab
         var promptQuestion = instantiatedPromptObject.GetComponentInChildren<TextMeshPro>();
-        
+        //Sets the text element of the prefab to the question specified for that type.
         promptQuestion.text = allPromptQuestions[type];
-        print(allPromptQuestions[type]);
+        
 
         return 0;
 
     }
-
+    /// <summary>
+    /// Based on given promptype returns series of prompt objects
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     private PromptOption[] GetPromptOptions(PromptType type)
     {
         switch (type)
@@ -76,6 +95,11 @@ public class PromptHandler : MonoBehaviour
         }
         return  null;
     }
+    /// <summary>
+    /// /// Based on given promptype returns series of prompt question
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     private string GetPromptQuestion(PromptType type)
     {
         switch (type)

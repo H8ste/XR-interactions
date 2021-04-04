20511ed021c13476cdbe99b5f2260290e419441f
BurmasGG <guitartard@hotmail.com>
2021-04-04 15:42:20 +0200

    WIP adding funcitonality for spawning prompt options.

--- MRTK6.1v2/Assets/PromptHandler.cs
+++ MRTK6.1v2/Assets/PromptHandler.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using System.Threading.Tasks;
 
-// TODO:: Missing functionality that, one prompt option "selection", prompthandler should return which option
+// TODO:: Missing functionality that, on prompt option "selection", prompthandler should return which option
 // TODO:: was selected to whatever entity started the prompthandler
 
 public class PromptHandler : ScriptableObject
@@ -16,17 +16,23 @@ public class PromptHandler : ScriptableObject
     Dictionary<PromptType, string> allPromptQuestions = new Dictionary<PromptType, string>();
 
     // Used to load in the necesarry prefab
+    GameObject promptQuestionPrefab;
     GameObject promptOptionPrefab;
 
     // Used to save the current instance of the prompt
-    GameObject instantiatedPromptObject;
+    GameObject instantiatedPromptQuestionObject;
+    GameObject instantiatedPromptOptionPrefab;
 
+    private ScrollHandler scrollhandler;
     /// <summary>
     /// Constructor for PromptHandler
     /// </summary>
     public PromptHandler()
     {
-        promptOptionPrefab = (GameObject)(Resources.Load("Prefabs/PromptPrefab"));
+        promptQuestionPrefab = (GameObject)(Resources.Load("Prefabs/PromptQuestionQuestionPrefab"));
+        if (!promptOptionPrefab) Debug.LogError("Prefab PromptQuestionPrefab does not exist");
+        promptOptionPrefab = (GameObject)(Resources.Load("Prefabs/PromptOptionPrefab"));
+        if (!promptOptionPrefab) Debug.LogError("Prefab PromptOptionPrefab does not exist");
 
         //Populates the Dictionaries with prompt types and their corresponding keys
         foreach (PromptType promptType in (PromptType[])Enum.GetValues(typeof(PromptType)))
@@ -46,22 +52,43 @@ public class PromptHandler : ScriptableObject
     public async Task<int> ShowPrompt(PromptType type)//Function is a WIP
     {
         // Sets the instance of the prompt object to the prefab
-        instantiatedPromptObject = Instantiate(promptOptionPrefab);
+        instantiatedPromptQuestionObject = Instantiate(promptQuestionPrefab);
+        instantiatedPromptOptionPrefab = Instantiate(promptOptionPrefab);
+
 
         if (allPromptQuestions.ContainsKey(type) && allPromptQuestions.TryGetValue(type, out string questionToAsk))
         {
             // Sets the text element of the prefab to the question specified for that type.
-            var instantiatedQuestionTextMesh = instantiatedPromptObject.GetComponentInChildren<TextMeshPro>();
+            var instantiatedQuestionTextMesh = instantiatedPromptQuestionObject.GetComponentInChildren<TextMeshPro>();
             if (instantiatedQuestionTextMesh) instantiatedQuestionTextMesh.text = questionToAsk;
         }
 
+
+        if (allPromptOptions.ContainsKey(type))
+        {
+            PromptOption[] retrievedoptions;
+            retrievedoptions = GetPromptOptions(type);
+            foreach (var promptOption in retrievedoptions)
+            {
+                Debug.Log("Setting properties...");
+
+            }
+
+
+
+
+
+        }
         // TODO:: set the text element of the prefab to options specified for that type
 
         return 0;
     }
 
     /* Private Methods */
-
+    private void SetPropertiesOnOptions(PromptOption promptOption)
+    {
+        var optionText = promptOption.InstatiatedScrollableItem.GetcomponentInChildren<TextMeshPro>();
+    }
     /// <summary>
     /// Gets the respective options for the given PromptType
     /// </summary>

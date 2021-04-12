db66f4d78e111dff79220abe473d342fec2f4ed0
h8ste <nolse16@student.aau.dk>
2021-04-12 16:45:30 +0200

    Added statehandler + statetypes (play/login/etc.). Missing apiHandler + loginHandler

---
+++ MRTK6.1v2/Assets/StateHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    DataHandler dataHandler;
+    TabHandler tabHandler;
+
+    StateType currentState = StateType.Login;
+
+    void Start()
+    {
+        // instantiate apiHandler
+
+        // instantiate datahandler
+        this.dataHandler = new DataHandler( /* apiHandler */);
+
+        // instantiate tabhandler
+        this.tabHandler = gameObject.AddComponent<TabHandler>().Instantiate(dataHandler);
+
+        // instantiate loginHandler -- then wait for login
+
+
+    }
+
+    void OnLogin()
+    {
+        currentState = StateType.Play;
+
+        dataHandler.FetchRoundInfo();
+
+        currentState = StateType.Play;
+
+        tabHandler.SwitchTab(TabType.PickingHandler);
+    }
+}

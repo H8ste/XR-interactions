92302785483618203b94ad862e1dfa56bee1172a
h8ste <nolse16@student.aau.dk>
2021-04-13 10:34:53 +0200

    Added vuforia to project + scanner scene

---
+++ MRTK6.1v2/Assets/ZXingDecoder.cs
@@ -0,0 +1,72 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using ZXing;
+
+public class ZXingDecoder : IDecoder
+{
+    private BarcodeReader Scanner { get; }
+
+    public ZXingDecoder()
+    {
+        Scanner = new BarcodeReader
+        {
+            AutoRotate = true,
+            TryInverted = false,
+            Options = { TryHarder = false, PossibleFormats = new List<BarcodeFormat>() { BarcodeFormat.QR_CODE } }
+        };
+    }
+
+    public string[] DecodeMultiple(byte[] rawRgb, int width, int height, RGBLuminanceSource.BitmapFormat format)
+    {
+        if (rawRgb == null || rawRgb.Length == 0 || width == 0 || height == 0)
+        {
+            return null;
+        }
+
+
+        string[] returnValue = null;
+
+        try
+        {
+            var result = Scanner.DecodeMultiple(rawRgb, width, height, format);
+            returnValue = result?.Select(item => item.Text).ToArray();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+
+
+        return returnValue;
+    }
+
+    public string Decode(Color32[] colors, int width, int height)
+    {
+        if (colors == null || colors.Length == 0 || width == 0 || height == 0)
+        {
+            return null;
+        }
+
+        string value = null;
+
+        try
+        {
+            //Scanner.DecodeMultiple()
+            var result = Scanner.Decode(colors, width, height);
+
+            if (result != null)
+            {
+                value = result.Text;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+
+        return value;
+    }
+}
